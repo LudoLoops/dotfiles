f907f09651f2d4899c9214971f70a781fdce58b7
ludoloops <ludoloops@pm.me>
2025-07-03 17:28:46 +0800

    update color nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,4 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
   {
     "folke/tokyonight.nvim",
     lazy = false,
@@ -19,7 +16,7 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
       require("kanagawa").setup({
-        compile = false, -- enable compiling the colorscheme
+        compile = true, -- enable compiling the colorscheme
         transparent = true, -- do not set background color
         terminalColors = false, -- define vim.g.terminal_color_{0,17}
         colors = {
@@ -41,12 +38,12 @@ return {
             -- Save an hlgroup with dark background and dimmed foreground
             -- so that you can use it where your still want darker windows.
             -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            -- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
             -- Popular plugins that open floats will link to NormalFloat by default;
             -- set their background accordingly if you wish to keep them dark and borderless
-            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            -- MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           }
         end,
       })
