dac79f6875800a04fc249dbeae2cf458cf225968
ludoloops <ludoloops@pm.me>
2025-07-03 15:48:26 +0800

    update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-if vim.g.neovide then
-  -- Put anything you want to happen only in Neovide here
-  vim.cmd("highlight Normal guibg=#141A21")
-  vim.cmd("highlight NormalNC guibg=#141A21")
-end
