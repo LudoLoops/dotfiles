afa3165fdf825eaeadf810bfb98bee8e3c4fa6fa
ludoloops <ludoloops@pm.me>
2025-07-03 17:16:00 +0800

    putback previous config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.wrap = true
+
+-- Automatically show diagnostics when there's an error under the cursor after a short pause
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { scope = "cursor" })
+  end,
+  desc = "Automatically show diagnostics under the cursor",
+})
+
+-- Optional: Adjust the delay before triggering (default is 4000 ms)
+vim.o.updatetime = 1000 -- Delay of 1000 ms before 'CursorHold' is triggered
