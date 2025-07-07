17d894632314532c003eebd2141951803bb2e92c
ludoloops <ludoloops@pm.me>
2025-07-07 13:29:15 +0800

    added oil + edit snack to open on right side

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_refresh_rate = 144
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_cursor_hack = true
+  vim.g.neovide_cursor_vfx_mode = "torpedo"
+  -- vim.cmd("highlight Normal guibg=#141A21")
+  -- vim.cmd("highlight NormalNC guibg=#141A21")
+end
