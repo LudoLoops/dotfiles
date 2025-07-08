eb1f200d758f4ec1a1ce1f71a5a1f66d2da419e6
ludoloops <ludoloops@pm.me>
2025-07-09 00:14:47 +0800

    feat: update nvim color + fish bg

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -16,7 +16,7 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
       require("kanagawa").setup({
-        compile = true, -- enable compiling the colorscheme
+        compile = false, -- enable compiling the colorscheme
         -- transparent = true, -- do not set background color
         terminalColors = false, -- define vim.g.terminal_color_{0,17}
         colors = {
@@ -31,19 +31,26 @@ return {
         overrides = function(colors)
           local theme = colors.theme
           return {
+            -- Flottants
             NormalFloat = { bg = "none" },
             FloatBorder = { bg = "none" },
             FloatTitle = { bg = "none" },
 
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            -- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+            -- Fond général
+            Normal = { fg = "#a6c7bd", bg = "#141A21" },
+            -- CursorLine = { bg = "#1F1F28" },
 
-            -- Popular plugins that open floats will link to NormalFloat by default;
-            -- set their background accordingly if you wish to keep them dark and borderless
-            -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            -- MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            -- Composants syntaxiques
+            Comment = { fg = "#748b84", italic = true },
+            --Identifier = { fg = "#C99359" }, -- variables, parameters
+            -- Statement = { fg = "#F2A05C" }, -- if, for, return
+            -- Type = { fg = "#0D998A" }, -- int, string, etc.
+            -- Constant = { fg = "#C2654A" }, -- numbers, true, false
+            -- Keyword = { fg = "#23867C" }, -- function, local, etc.
+            -- Function = { fg = "#F2A05C" }, -- function names
+            ["@keyword.import"] = { fg = "#70628C" }, -- couleur pour `import`, `from`, etc.
+            ["@include"] = { fg = "#70628C" }, -- parfois utilisé aussi
+            -- Keyword = { fg = "#23867C" }, -- fallback classique
           }
         end,
       })
