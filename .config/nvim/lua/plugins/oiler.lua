dcd9f85bb5a969bdbd26f7fccfd8d864c5d173ff
Ludo Loops <56033812+LudoLoops@users.noreply.github.com>
2025-12-17 07:46:03 +0800

    feat: add oil file explorer with automatic preview window (#8)
    Configure oil.nvim with:
    - Keymap '-' to open parent directory explorer
    - Automatic preview window on startup
    - Fast scratch preview method with cursor tracking

--- .config/nvim/lua/plugins/oiler.lua
+++ .config/nvim/lua/plugins/oiler.lua
@@ -2,10 +2,40 @@ return {
   "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    -- Configuration for the file preview window
+    preview_win = {
+      -- Whether the preview window is automatically updated when the cursor is moved
+      update_on_cursor_moved = true,
+      -- How to open the preview window "load"|"scratch"|"fast_scratch"
+      preview_method = "fast_scratch",
+      -- A function that returns true to disable preview on a file e.g. to avoid lag
+      disable_preview = function(filename)
+        return false
+      end,
+      -- Window-local options to use for preview window buffers
+      win_options = {},
+    },
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function(_, opts)
+    require("oil").setup(opts)
+    -- Afficher le preview automatiquement quand Oil s'ouvre
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "oil",
+      callback = function()
+        vim.defer_fn(function()
+          local util = require("oil.util")
+          -- Vérifier que le preview n'existe pas déjà
+          if not util.get_preview_win() then
+            require("oil.actions").preview.callback()
+          end
+        end, 100)
+      end,
+    })
+  end,
 }
