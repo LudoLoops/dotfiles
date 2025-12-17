a15ea4fe82121134f78bd48e1732699e87e9daea
Ludo Loops <ludoloops@pm.me>
2025-12-17 01:35:02 +0100

    fix: remove config function from Continue plugin

--- .config/nvim/lua/plugins/continue.lua
+++ .config/nvim/lua/plugins/continue.lua
@@ -3,14 +3,4 @@ return {
   dependencies = {
     "rcarriga/nvim-notify",
   },
-  event = "VeryLazy",
-  config = function()
-    require("continue").setup({
-      enable_telemetry = false,
-      database = {
-        "sqlite",
-      },
-      allow_anonymous_telemetry = false,
-    })
-  end,
 }
