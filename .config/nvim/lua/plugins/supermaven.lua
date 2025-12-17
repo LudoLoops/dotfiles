927d752b49289cdc0a383d81cba530b0ac1b80b6
Ludo Loops <ludoloops@pm.me>
2025-12-17 01:18:11 +0100

    fix: correct Supermaven plugin repository name

--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,8 +1,8 @@
 return {
-  "supermaven-ai/supermaven-nvim",
+  "supermaven-inc/supermaven-nvim",
   event = "VeryLazy",
   config = function()
-    require("supermaven-ai.supermaven-nvim").setup({
+    require("supermaven-nvim").setup({
       keymaps = {
         accept_suggestion = "<Tab>",
         clear_suggestion = "<C-]>",
