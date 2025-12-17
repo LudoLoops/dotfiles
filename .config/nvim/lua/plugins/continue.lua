371fd7cb2666ad180c2a14557cb364d0363ecfbc
Ludo Loops <ludoloops@pm.me>
2025-12-17 01:28:41 +0100

    feat: add Continue.dev AI assistant plugin

---
+++ .config/nvim/lua/plugins/continue.lua
@@ -0,0 +1,17 @@
+return {
+  "continue-dev/continue",
+  build = "cd continue-ui && npm install && npm run build",
+  dependencies = {
+    "rcarriga/nvim-notify",
+  },
+  event = "VeryLazy",
+  config = function()
+    require("continue").setup({
+      enable_telemetry = false,
+      database = {
+        "sqlite",
+      },
+      allow_anonymous_telemetry = false,
+    })
+  end,
+}
