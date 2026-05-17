4cdcc38c8d5a281bebc1158a1317de5d921fa54d
Mako <mako@lelab.dev>
2026-05-17 23:08:09 +0200

    zellij: remove aether layout, use default for both machines

--- .config/fish/functions/aether.fish
+++ .config/fish/functions/aether.fish
@@ -3,8 +3,8 @@
 # Default session: hermes
 function aether
     set -l session hermes
-    if test (count $argv) -gt 0
-        set session $argv[1]
+    if test (count \$argv) -gt 0
+        set session \$argv[1]
     end
-    ssh aether -t "zellij -l aether attach -c $session"
+    ssh aether -t "zellij -l default attach -c \$session"
 end
